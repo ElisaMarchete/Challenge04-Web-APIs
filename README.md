988d1e270c7f3a98dd1b0093eaed53cb2f9a80e9
Joao Marchete <elisamarchete@gmail.com>
2023-03-30 21:36:36 -0400

    final commit - quiz completed

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Challenge04-Web-APIs
-04 Web APIs: Code Quiz
+# Code Quiz
+
+Welcome to the README file for the Code Quiz!
+
+## Description
+This code quiz is designed to test your knowledge of programming concepts and syntax in a specific programming language. In this quiz, you will be presented with a series of multiple-choice questions and coding challenges. You will have a limited amount of time to answer each question, and your score will be calculated based on the number of correct answers you provide.
+
+## Table of Contents 
+1. To start the quiz, open the index.html file in your web browser.
+2. Select Start button to start the quiz.
+3. Read each question carefully and select the answer you think is correct, every incorrect answer will deduct 10 seconds from your time.
+4. Once you have completed the quiz, your score will be displayed on the final page.
+5. You can save your score by entering your initials in the input field and clicking the Save button.
+6. You can delete your score by clicking the clear button.
+
+## Files:
+index.html: This is the main HTML file for the quiz.
+style.css: This file contains the styles for the quiz.
+script.js: This file contains the JavaScript code that runs the quiz.
+
+Good luck and have fun!
+
+- License: MIT
+
+## Screenshot and link for the deployed application
+![Screenshot of deployed application](./assets/screenshot/Screenshot1.png)(./assets/screenshot/Screenshot2.png)
+[Link deployed application]()
--- assets/scrip.js
+++ assets/scrip.js
@@ -1,224 +1,222 @@
 // Control the next question
-let countQuestion = 0;
-let getQuestion;
-let time = 60;
-let timeInterval;
-let timerEl;
-let nameScore;
-let score;
+let countQuestion = 0
+let getQuestion
+let time
+let timeInterval
+let timerEl
+let nameScore
+let score
 
 // Assignment Code
-let startQuizBtn = document.querySelector(".startQuiz");
-let questionParag = document.getElementById("questionParag");
-let choiceBtn1 = document.getElementById("btn1");
-let choiceBtn2 = document.getElementById("btn2");
-let choiceBtn3 = document.getElementById("btn3");
-let choiceBtn4 = document.getElementById("btn4");
-let answerEl = document.getElementById("answer");
-let containerEl = document.querySelector(".container");
-let hideContentEl = document.getElementById("hideContent");
-let initials = document.getElementById("initials");
-let submitInitials = document.getElementById("submitInitials");
-let savedInitScore = document.getElementById("savedInitScore");
-let errorMsg = document.getElementById("errorMsg");
-let timerElement = document.getElementById("timerCount");
-let doneMsg = document.querySelector(".storeInfo");
-let timeParag = document.getElementById("timeParag");
-let scoreStorage = document.getElementById("score");
-let highScores = document.querySelector(".savedInfo");
-let clearBtn = document.getElementById("clear");
-let btnGoBack = document.getElementById("btnGoBack");
+let startQuizBtn = document.querySelector('.startQuiz')
+let questionParag = document.getElementById('questionParag')
+let choiceBtn1 = document.getElementById('btn1')
+let choiceBtn2 = document.getElementById('btn2')
+let choiceBtn3 = document.getElementById('btn3')
+let choiceBtn4 = document.getElementById('btn4')
+let answerEl = document.getElementById('answer')
+let containerEl = document.querySelector('.container')
+let hideContentEl = document.getElementById('hideContent')
+let initials = document.getElementById('initials')
+let submitInitials = document.getElementById('submitInitials')
+let savedInitScore = document.getElementById('savedInitScore')
+let errorMsg = document.getElementById('errorMsg')
+let timerElement = document.getElementById('timerCount')
+let doneMsg = document.querySelector('.storeInfo')
+let timeParag = document.getElementById('timeParag')
+let scoreStorage = document.getElementById('score')
+let highScores = document.querySelector('.savedInfo')
+let btnGoBack = document.getElementById('btnGoBack')
+let btnClear = document.getElementById('btnClear')
+let savedInfo = document.querySelector('.savedInfo')
 
 // Add event listener to generate button
-startQuizBtn.addEventListener("click", startGame);
+startQuizBtn.addEventListener('click', startGame)
 
 // add event listener to move to the nextquestion
-choiceBtn1.addEventListener("click", nextQuestion);
-choiceBtn2.addEventListener("click", nextQuestion);
-choiceBtn3.addEventListener("click", nextQuestion);
-choiceBtn4.addEventListener("click", nextQuestion);
+choiceBtn1.addEventListener('click', nextQuestion)
+choiceBtn2.addEventListener('click', nextQuestion)
+choiceBtn3.addEventListener('click', nextQuestion)
+choiceBtn4.addEventListener('click', nextQuestion)
 // Get player name
-submitInitials.addEventListener("click", initialsInput);
+submitInitials.addEventListener('click', initialsInput)
 
-btnGoBack.addEventListener("click", startGame);
+btnGoBack.addEventListener('click', initialScreen)
 
-// The init function is called when the page loads FIXE IT
-function init() {
-  let nameScore;
+btnClear.addEventListener('click', clearLocalStorage)
+
+function initialScreen() {
+  hideContentEl.style.display = 'block'
+  containerEl.style.display = 'none'
+  savedInfo.style.display = 'none'
+  savedInitScore.style.display = 'block'
+
+  savedInitScore.innerHTML = ''
 }
-
 function startGame() {
-  countQuestion = 0;
-  time = 60;
-  timeInterval = setInterval(timer, 1000);
-  createQuestion();
-  displayContent();
-  hideHeader();
+  countQuestion = 0
+  time = 90
+  timerElement.style.display = 'block'
+
+  timeInterval = setInterval(timer, 1000)
+  createQuestion()
+  displayContent()
+  hideHeader()
 }
 
 function timer() {
-  time = time - 1;
-  timerEl = timerElement.textContent = time;
+  time = time - 1
+  timerEl = timerElement.textContent = time
   if (time <= 0) {
-    clearInterval(timeInterval);
+    clearInterval(timeInterval)
   }
 }
 
 function nextQuestion(event) {
-  let getResponse = getQuestion.correctAnswer;
-  let selectedChoice = event.target.textContent;
+  let getResponse = getQuestion.correctAnswer
+  let selectedChoice = event.target.textContent
 
   // check if the answer is correct
   if (selectedChoice == getResponse) {
-    answerEl.innerHTML = "Correct Answer!";
+    answerEl.innerHTML = 'Correct Answer!'
   } else {
-    answerEl.innerHTML = "Incorrect Answer!";
-    time = time - 10;
+    answerEl.innerHTML = 'Incorrect Answer!'
+    time = time - 10
   }
 
   // move tp the next question
-  countQuestion++;
+  countQuestion++
 
   if (countQuestion >= 7) {
-    clearInterval(timeInterval);
-    doneMsg.style.display = "block";
-    timerElement.style.display = "none";
-    timeParag.style.display = "none";
-    answerEl.style.display = "none";
+    clearInterval(timeInterval)
+    doneMsg.style.display = 'block'
+    timerElement.style.display = 'none'
+    timeParag.style.display = 'none'
+    answerEl.style.display = 'none'
 
-    // localStorage.setItem("storeTimer", timerEl);
-    // let getTimer = localStorage.getItem("storeTimer");
-    // score = scoreStorage.textContent = getTimer;
+    score = scoreStorage.textContent = time
   }
 
   // Call function to create a qustion when one option is selected
-  createQuestion();
+  createQuestion()
 }
 
 // Display question, choices, and response
 function createQuestion() {
-  getQuestion = questions[countQuestion];
+  getQuestion = questions[countQuestion]
   questionParag.textContent = getQuestion.question;
 
-  choiceBtn1.textContent = getQuestion.options[0];
-  choiceBtn2.textContent = getQuestion.options[1];
-  choiceBtn3.textContent = getQuestion.options[2];
-  choiceBtn4.textContent = getQuestion.options[3];
+  choiceBtn1.textContent = getQuestion.options[0]
+  choiceBtn2.textContent = getQuestion.options[1]
+  choiceBtn3.textContent = getQuestion.options[2]
+  choiceBtn4.textContent = getQuestion.options[3]
 }
 
 // Display hide questions
 function displayContent() {
-  if (containerEl.style.display == "none") {
-    containerEl.style.display = "block";
+  if (containerEl.style.display == 'none') {
+    containerEl.style.display = 'block'
   } else {
-    containerEl.style.display = "none";
+    containerEl.style.display = 'none'
   }
 }
 
 // Hide header
 function hideHeader() {
-  if (hideContentEl.style.display === "block") {
-    hideContentEl.style.display = "none";
+  if (hideContentEl.style.display === 'block') {
+    hideContentEl.style.display = 'none'
   } else {
-    hideContentEl.style.display = "block";
+    hideContentEl.style.display = 'block'
   }
 }
 
 function initialsInput(event) {
-  event.preventDefault();
+  event.preventDefault()
   let userInitials = initials.value;
-console.log("initial")
-  if (userInitials === "") {
-    errorMsg.innerHTML = "ERROR! Cannot be blank";
-  } else {
-    errorMsg.innerHTML = "Registered successfully";
-  }
 
-  let storeResult = JSON.parse(localStorage.getItem("store"));
-  let finalScore = `${userInitials} - ${time}`;
+  let storeResult = JSON.parse(localStorage.getItem('store'))
+  let finalScore = `${userInitials} ${time}`
   if (!storeResult) {
-    storeResult = [finalScore];
+    storeResult = [finalScore]
   } else {
-    storeResult.push(finalScore);
+    storeResult.push(finalScore)
   }
 
   let saveResult = JSON.stringify(storeResult)
-  localStorage.setItem("store", saveResult);
+  localStorage.setItem('store', saveResult)
+
+  clearInterval(timeInterval)
 
   // let getInit = localStorage.getItem("storeInitials");
   for (let i = 0; i < storeResult.length; i++) {
-    savedInitScore.textContent += storeResult[i];
-
-    
+    savedInitScore.innerHTML += `<li>${storeResult[i]}</li>`
   }
-
-
-  
-
-  highScores.style.display = "block";
-  doneMsg.style.display = "none";
+  highScores.style.display = 'block'
+  doneMsg.style.display = 'none'
 }
 
-// Calls init() so that it fires when page opened
-init();
+function clearLocalStorage() {
+  let clear = localStorage.removeItem('store')
+  if (!clear) {
+    savedInitScore.style.display = 'none'
+  }
+}
 
 // Array of questions, choices, and answers
 let questions = [
   {
-    question: "What does HTML stand for?",
+    question: 'What does HTML stand for?',
     options: [
-      "Home Tool Markup Language",
-      "Hyperlinks and Text Markup Language",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
+      'Home Tool Markup Language',
+      'Hyperlinks and Text Markup Language',
+      'Hyper Text Markup Language',
+      'Hyper Text Multiple Language',
     ],
-    correctAnswer: "Hyper Text Markup Language",
+    correctAnswer: 'Hyper Text Markup Language',
   },
   {
-    question: "What is the correct HTML element for inserting a line break?",
-    options: ["<br>", "<lb>", "<line>", "<break>"],
-    correctAnswer: "<br>",
+    question: 'What is the correct HTML element for inserting a line break?',
+    options: ['<br>', '<lb>', '<line>', '<break>'],
+    correctAnswer: '<br>',
   },
   {
-    question: "What does CSS stand for?",
+    question: 'What does CSS stand for?',
     options: [
-      "colorful style sheets",
-      "cascading style sheets",
-      "computer style sheets",
-      "creative style sheets",
+      'colorful style sheets',
+      'cascading style sheets',
+      'computer style sheets',
+      'creative style sheets',
     ],
-    correctAnswer: "cascading style sheets",
+    correctAnswer: 'cascading style sheets',
   },
   {
-    question: "Which HTML tag is used to define an internal style sheet?",
-    options: ["<style>", "<html>", "<script>", "<css>"],
-    correctAnswer: "<style>",
+    question: 'Which HTML tag is used to define an internal style sheet?',
+    options: ['<style>', '<html>', '<script>', '<css>'],
+    correctAnswer: '<style>',
   },
   {
-    question: "Inside which HTML element do we put the JavaScript?",
-    options: ["<scripting>", "<js>", "<script>", "<javascript>"],
-    correctAnswer: "<script>",
+    question: 'Inside which HTML element do we put the JavaScript?',
+    options: ['<scripting>', '<js>', '<script>', '<javascript>'],
+    correctAnswer: '<script>',
   },
   {
-    question: "Where is the correct place to insert a JavaScript?",
+    question: 'Where is the correct place to insert a JavaScript?',
     options: [
-      "The <footer> section",
-      "The <head> section",
-      "The <body> section",
-      "Both the <head> section and the <body> section are correct",
+      'The <footer> section',
+      'The <head> section',
+      'The <body> section',
+      'Both the <head> <body> section',
     ],
-    correctAnswer: "The <body> section",
+    correctAnswer: 'The <body> section',
   },
   {
-    question: "How do you create a function in JavaScript?",
+    question: 'How do you create a function in JavaScript?',
     options: [
-      "function = myFunction()",
-      "function myFunction()",
-      "function:myFunction()",
-      "function ()",
+      'function = myFunction()',
+      'function myFunction()',
+      'function:myFunction()',
+      'function ()',
     ],
-    correctAnswer: "function myFunction()",
+    correctAnswer: 'function myFunction()',
   },
-];
-
-// Hide start screen
+]
